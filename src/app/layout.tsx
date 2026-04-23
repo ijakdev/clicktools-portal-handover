81dcb7fb60043a7ca64605638d8dbf4b70618b4d
xoehddl2002 <xoehddl2002@ijaksnc.com>
2026-04-23 15:22:27 +0900

    `feat(layout): SEO 메타태그 및 오픈그래프 설정 추가`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,10 +42,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className="scroll-smooth">
-      <body
+    <head>
+      <title>이미지, PDF, 계산까지 한 번에 해결 한국형 올인원 무료 유틸 플랫폼, 클릭툴스</title>
+
+      <meta name="description" content="이미지 리사이즈, PDF 변환, 텍스트 비교, QR코드 생성까지 무료로 바로 사용하는 웹 기반 유틸리티 서비스" />
+      <meta name="keywords" content="유틸리티, 이미지 압축, PDF 변환, QR 생성, 숏 URL, 텍스트 비교, 클릭툴스, 한국형 유틸리티" />
+
+      {/*Open Graph*/}
+      <meta property="og:title" content="이미지, PDF, 계산까지 한 번에 해결 한국형 올인원 무료 유틸 플랫폼, 클릭툴스" />
+      <meta property="og:description" content="이미지 리사이즈, PDF 변환, 텍스트 비교, QR코드 생성까지 무료로 바로 사용하는 웹 기반 유틸리티 서비스" />
+      <meta property="og:url" content="https://www.clicktools.co.kr" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="ko_KR" />
+
+      {/* Google Verification */}
+      <meta name="google-site-verification" content="tX3a-H31R90V0205YdyW186kvY6dxOqqI_9d-A7F9-o" />
+
+      {/* Icons*/}
+      <link rel="icon" href="/icon.png" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+
+      {/*에드센스 링크*/}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2350957189788653"
+              crossOrigin="anonymous"></script>
+    </head>
+    <body
         className={`${inter.variable} ${outfit.variable} antialiased font-sans bg-slate-50 text-slate-900 min-h-screen`}
-      >
-        <DynamicLayoutWrapper>
+    >
+
+    <DynamicLayoutWrapper>
           {children}
         </DynamicLayoutWrapper>
         <Toaster richColors position="top-center" />
